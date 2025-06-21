014c1fbfada5ebe695e36dd9c53827b233fbabba
MaryAngeleen16 <angelpagalan38@gmail.com>
2025-06-21 14:44:47 +0800

    allowerd origin

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,33 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5000',
+  'https://notable-complete-garfish.ngrok-free.app',
+  'https://opticoolweb-backend.onrender.com',
+  'https://opticoolweb-backend.onrender.com/api/v1',
+  'https://opticoolprediction.onrender.com'
+];
+
 // Middleware setup
 app.use(express.json());
-app.use(cors());
+
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // allow requests with no origin (like mobile apps, curl, etc.)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
